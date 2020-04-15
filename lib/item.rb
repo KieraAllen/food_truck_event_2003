2bd5ab1302fa4495d2671b2e82374f039d9b5dd4
Kiera Allen <kieraaallen@gmail.com>
2020-04-15 10:25:32 -0600

    Pass attributes test

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,11 @@
 class Item
+  attr_reader :item_info,
+              :name,
+              :price
 
   def initialize(item_info)
     @item_info = item_info
+    @name = item_info[:name]
+    @price = item_info[:price][1..-1].to_f
   end
 end
