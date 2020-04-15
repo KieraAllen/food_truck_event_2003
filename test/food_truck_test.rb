8139cf9e0765be84077c9f65877445ab6d1e2f2c
Kiera Allen <kieraaallen@gmail.com>
2020-04-15 10:29:11 -0600

    Test and add FoodTruck instance existance

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -4,4 +4,13 @@ require './lib/item'
 require './lib/food_truck'
 
 class FoodTruckTest < Minitest::Test
+
+  def setup
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists
+    assert_instance_of FoodTruck, @food_truck
+  end
+
 end
