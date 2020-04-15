10c609d600268f2a1b2346f86034f95168e75c3a
Kiera Allen <kieraaallen@gmail.com>
2020-04-15 10:10:49 -0600

    Test and add Item instance existance

---
+++ lib/item.rb
@@ -0,0 +1,6 @@
+class Item
+
+  def initialize(item_info)
+    @item_info = item_info
+  end
+end
--- test/item_test.rb
+++ test/item_test.rb
@@ -9,5 +9,9 @@ class ItemTest < Minitest::Test
   @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
   @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
-  
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+  end
+
 end
