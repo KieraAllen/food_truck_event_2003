d97603b265595d8033d73d3c0c50ca6a07560ad2
Kiera Allen <kieraaallen@gmail.com>
2020-04-15 10:55:38 -0600

    Create stock test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,5 +25,13 @@ class FoodTruckTest < Minitest::Test
     assert_equal 0, @food_truck.check_stock(@item1)
   end
 
+  def test_it_can_stock
+    @food_truck.stock(@item1, 30)
+
+    expected = {@item1 => 30}
+
+    assert_equal expected, @food_truck.inventory
+  end
+
 
 end
