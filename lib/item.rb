b73366a104df49379c3a9a1cd9395b9b24394c7e
Kiera Allen <kieraaallen@gmail.com>
2020-04-15 10:40:43 -0600

    Change item_info instance variable to item

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,10 @@
 class Item
-  attr_reader :item_info,
+  attr_reader :item,
               :name,
               :price
 
   def initialize(item_info)
-    @item_info = item_info
+    @item = item_info
     @name = item_info[:name]
     @price = item_info[:price][1..-1].to_f
   end
